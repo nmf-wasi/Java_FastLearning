098d800e37049f529bd026a5940f135e1d1b8b84
NMF Wasi <wasi7741@gmail.com>
2026-01-12 03:35:07 +0900

    Printf

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,29 @@
 import java.util.Random;
 public class Main {
-    public static void main() {
-//  Random numbers
+    public static void main(String[] args) {
+//  PrintF
 
 
-        Random random=new Random();
+        String name="Wasi";
+        char firstLetter='W';
+        int age=19;
+        double height=1520000.3;
+        boolean isLoser=true;
 
-        int number1;
-//          number=random.nextInt(); // gives a huge number
-        number1=random.nextInt(1,5+1); // will give a number between 1-5
-        System.out.println(number1);
+        System.out.printf("Hello %s, %c, %d, %.2f, %b", name, firstLetter, age, height, isLoser);
 
-//        random doubles
+        System.out.printf("\n+%,.2f", height);
+        System.out.printf("\n% .3f", height);
+        int id1=1;
+        int id2=23;
+        int id3=456;
+        int id4=7890;
+        // setting width
+        System.out.printf("\n%4d", id1);
+        System.out.printf("\n%04d", id2);
+        System.out.printf("\n%-4d", id3);
+        System.out.printf("\n%d", id4);
 
-        double number2= random.nextDouble();
-        System.out.println(number2);
-        // random boolean
-        boolean num3=random.nextBoolean();
-        System.out.println(num3);
-
-        if(random.nextBoolean()) System.out.println("HEADS!");
-        else System.out.println("TAILS!");
     }
 
 }
