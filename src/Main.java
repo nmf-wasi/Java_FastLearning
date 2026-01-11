b77b7fe1f162b147a6bec6a78002d8c244806826
NMF Wasi <wasi7741@gmail.com>
2026-01-11 22:31:59 +0900

    user input

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main() {
-        //     1. declaring var
-        int var_name;
-        //    2. initializing Var
-        int var_name2 = 12;
-        //    3. Assigning var
-        var_name = 15;
 
-        System.out.println(var_name);
-        System.out.println(var_name2);
+        Scanner scanner = new Scanner(System.in);
 
-//        var types:
-//        Short, int, long, float, double, String, boolean
-
-
-//        var= 2 types:
-//                    Primitive: Simple val stored directly in memory (stack) -> short, int float bye, double
-//                    Reference: Memory address (stack) that points to the (heap) -> string, array, obj
-
-
-//        printing:
-        System.out.println("The val is: " + var_name);
-//    double
-        double gpa=3.5;
-        System.out.println("gpa".toUpperCase()+gpa);
-        float price= 15.5f;
-        System.out.println("Price: "+price);
-
-        char grade='A';
-        System.out.println("Grade: "+grade);
-
-        char symbol='!';
-        System.out.println(symbol);
-
-        boolean isStudent=true;
-        System.out.println(isStudent);
-        System.out.println(!isStudent);
-
-        if(!isStudent)
-            System.out.println("Can't go in!");
-        else
-            System.out.println("Go in!");
-
-        String name="nmf_wasi";
+//        System.out.println("Enter your name: ");
+        System.out.print("Enter your name: ");
+//        String name=scanner.nextLine();  // takes in whole line
+        String name = scanner.next(); //takes in only one word
         System.out.println(name);
-        name="Kel";
-        System.out.println(name);
-        System.out.println("Hi, "+name+'!');
+//        System.out.print(name);
 
+        System.out.print("Enter your age: ");
+//        scanner.nextInt();//takes in next int
+        System.out.println(scanner.nextInt());
+
+        System.out.print("Enter your GPA: ");
+        double gpa=scanner.nextDouble();
+        System.out.println("GPA: "+gpa);
+
+        // common issue is to encounter an extra newline after numbers, if u use nextInt and then nextLine, then u will encounter this,
+        // to avoid this, use scanner.nextLine() before taking input of the line
+
+
+        scanner.close();
     }
 
 }
