e5d21c1b591db385ddc1dc11544632b898a17405
NMF Wasi <wasi7741@gmail.com>
2026-01-12 22:32:32 +0900

    Super

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
 public class Main {
     static void main(String[] args) {
 
-        var dog=new Dog();
-        var cat=new Cat();
-
-        dog.eat();
-        cat.eat();
-        dog.breathe();
-        cat.breathe();
-        cat.live();
-        Plant plant = new Plant();
-        plant.photosynthesize();
+        Person person = new Person("NMF", "Wasi");
+        person.showName();
+        Student student = new Student("nmf", "wasi", 4.2);
+        student.showName();
+        student.showGpa();
     }
-
 }
 
 
---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    String first;
+    String last;
+    Person(String first, String last){
+        this.first=first;
+        this.last=last;
+    }
+
+    void showName(){
+        System.out.println(first+" "+last);
+    }
+}
