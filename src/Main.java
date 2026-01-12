aa9c1f8cad0e2dd4689627914c33bfc7999c37dc
NMF Wasi <wasi7741@gmail.com>
2026-01-12 19:11:52 +0900

    arrays with user input

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,16 @@ public class Main {
 
 //        String [] hooman={"Wasi", "Lia", "Kel"};
 
-        String[]hooman=new String[3];
+        String[] hooman = new String[3];
 
-        Scanner scanner=new Scanner(System.in);
-        for (int i=0;i<3;i++){
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
             System.out.print("Enter the name of the hooman: ");
-            hooman[i]=scanner.nextLine();
+            hooman[i] = scanner.nextLine();
         }
-        for(String hoo : hooman)
+        for (String hoo : hooman)
             System.out.println(hoo);
-
+        scanner.close();
 
     }
 
