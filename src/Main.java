8ce6513210376e715855716057a0c11c6976a432
NMF Wasi <wasi7741@gmail.com>
2026-01-12 12:30:39 +0900

    enchanted switch

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,11 @@
 public class Main {
     static void main(String[] args) {
-//      .substring()= A method used to extract a portion of a string
-//        .substring(start, end)
 
-        String email = "nmf_wasi@yahhoo.com";
-        String username = email.substring(0, email.indexOf('@'));
-        String domain = email.substring(email.indexOf('@')+1 , email.lastIndexOf('m') + 1);
-        System.out.println(email);
-        System.out.println(username);
-        username=email.substring(email.indexOf('@'));
-        System.out.println(username);
-        System.out.println(domain);
-        domain = email.substring(email.indexOf('@')+1);
-        System.out.println(domain);
-
-        System.out.println(email.contains("@"));
-        System.out.println(email.contains("Wasi"));
-
+        String day="Monday";
+        switch (day){
+            case "Monday", "Tuesday", "Wednesday", "Thursday","Friday" -> System.out.println("Weekday >_<");
+            case "Saturday", "Sunday" -> System.out.println("Weekend ^--^");
+        }
     }
 
 }
