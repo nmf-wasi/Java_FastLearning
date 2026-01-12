3cacfc56983383e6d768361299bcb4f92f7bd26b
NMF Wasi <wasi7741@gmail.com>
2026-01-12 14:50:15 +0900

    arrays

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.lang.reflect.Array;
+
 public class Main {
     static void main(String[] args) {
 
-        String day="Monday";
-        switch (day){
-            case "Monday", "Tuesday", "Wednesday", "Thursday","Friday" -> System.out.println("Weekday >_<");
-            case "Saturday", "Sunday" -> System.out.println("Weekend ^--^");
+      String [] fruits ={"apple", "banana", "coconut"};
+        System.out.println(fruits);
+        System.out.println(fruits[0]);
+        fruits[2]="Pineapple";
+        System.out.println(fruits[2]);
+
+
+        System.out.println(fruits.length);
+
+        for(String fruit :fruits) {
+            System.out.println(fruit);
         }
     }
 
