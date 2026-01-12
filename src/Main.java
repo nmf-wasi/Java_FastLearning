77b86db2167c335da0dda508e8addf6b37f5afde
NMF Wasi <wasi7741@gmail.com>
2026-01-12 11:46:08 +0900

    sub string methods

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 public class Main {
     static void main(String[] args) {
-//      String methods
-    String name="Honey bunny sugar bunny ";
-        System.out.println(name.length());
-        System.out.println(name.toUpperCase());
-        System.out.println(name.toLowerCase());
-        System.out.println(name.charAt(0));
-        System.out.println(name.indexOf('W'));
-        System.out.println(name.indexOf('b'));
-        System.out.println(name.lastIndexOf('u'));
-        System.out.println(name.trim());
-        System.out.println(name.replace('y', 'i'));
-        System.out.println(name.isEmpty());
-        System.out.println(name.contains("bunn"));
-        System.out.println(name.equals("NMF Wasi"));
+//      .substring()= A method used to extract a portion of a string
+//        .substring(start, end)
 
+        String email = "nmf_wasi@yahhoo.com";
+        String username = email.substring(0, email.indexOf('@'));
+        String domain = email.substring(email.indexOf('@')+1 , email.lastIndexOf('m') + 1);
+        System.out.println(email);
+        System.out.println(username);
+        username=email.substring(email.indexOf('@'));
+        System.out.println(username);
+        System.out.println(domain);
+        domain = email.substring(email.indexOf('@')+1);
+        System.out.println(domain);
+
+        System.out.println(email.contains("@"));
+        System.out.println(email.contains("Wasi"));
 
     }
 
